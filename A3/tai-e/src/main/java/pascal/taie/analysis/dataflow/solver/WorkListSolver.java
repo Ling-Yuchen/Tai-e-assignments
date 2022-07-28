bc90a5408871859d72caa1563e629f25b3702f77
Yoson <2093439154@qq.com>
2022-07-28 16:27:43 +0800

    code from A1 & A2

--- A3/tai-e/src/main/java/pascal/taie/analysis/dataflow/solver/WorkListSolver.java
+++ A3/tai-e/src/main/java/pascal/taie/analysis/dataflow/solver/WorkListSolver.java
@@ -26,6 +26,9 @@ import pascal.taie.analysis.dataflow.analysis.DataflowAnalysis;
 import pascal.taie.analysis.dataflow.fact.DataflowResult;
 import pascal.taie.analysis.graph.cfg.CFG;
 
+import java.util.LinkedList;
+import java.util.List;
+
 class WorkListSolver<Node, Fact> extends Solver<Node, Fact> {
 
     WorkListSolver(DataflowAnalysis<Node, Fact> analysis) {
@@ -35,10 +38,44 @@ class WorkListSolver<Node, Fact> extends Solver<Node, Fact> {
     @Override
     protected void doSolveForward(CFG<Node> cfg, DataflowResult<Node, Fact> result) {
         // TODO - finish me
+        List<Node> workList = new LinkedList<>(cfg.getNodes());
+        while (!workList.isEmpty()) {
+            Node node = workList.get(0);
+            workList.remove(0);
+            if (!node.equals(cfg.getEntry())) {
+                for (Node pred : cfg.getPredsOf(node)) {
+                    analysis.meetInto(result.getOutFact(pred), result.getInFact(node));
+                }
+            }
+            if (analysis.transferNode(node, result.getInFact(node), result.getOutFact(node))) {
+                for (Node succ : cfg.getSuccsOf(node)) {
+                    if (!workList.contains(succ)) {
+                        workList.add(succ);
+                    }
+                }
+            }
+        }
     }
 
     @Override
     protected void doSolveBackward(CFG<Node> cfg, DataflowResult<Node, Fact> result) {
         // TODO - finish me
+        List<Node> workList = new LinkedList<>(cfg.getNodes());
+        while (!workList.isEmpty()) {
+            Node node = workList.get(0);
+            workList.remove(0);
+            if (!node.equals(cfg.getExit())) {
+                for (Node succ : cfg.getSuccsOf(node)) {
+                    analysis.meetInto(result.getInFact(succ), result.getOutFact(node));
+                }
+            }
+            if (analysis.transferNode(node, result.getInFact(node), result.getOutFact(node))) {
+                for (Node pred : cfg.getPredsOf(node)) {
+                    if (!workList.contains(pred)) {
+                        workList.add(pred);
+                    }
+                }
+            }
+        }
     }
 }
